172d3107800092f40bc334a1c75ed2cff4d421b6
EmilyRochaTascaSenai <emily.rocha192@gmail.com>
2024-09-24 10:50:39 -0300

    Fazendo a tela da calculadora e fazendo codigo para abrir a calculadora  depois do carregando

---
+++ Calculadora.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace Calculadora
+{
+    partial class Calculadora
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Calculadora));
+            Mais = new Button();
+            Menos = new Button();
+            Vezes = new Button();
+            Dividido = new Button();
+            igual = new Button();
+            CE = new Button();
+            Porcentagem = new Button();
+            Num1 = new Button();
+            Num2 = new Button();
+            Num3 = new Button();
+            Num4 = new Button();
+            Num5 = new Button();
+            Num6 = new Button();
+            Num7 = new Button();
+            Num8 = new Button();
+            Num9 = new Button();
+            Num0 = new Button();
+            Resultado = new TextBox();
+            SuspendLayout();
+            // 
+            // Mais
+            // 
+            Mais.BackColor = Color.FromArgb(255, 128, 0);
+            Mais.ForeColor = SystemColors.ControlText;
+            Mais.Location = new Point(614, 54);
+            Mais.Name = "Mais";
+            Mais.Size = new Size(83, 38);
+            Mais.TabIndex = 0;
+            Mais.Text = "+";
+            Mais.UseVisualStyleBackColor = false;
+            Mais.Click += button1_Click;
+            // 
+            // Menos
+            // 
+            Menos.BackColor = Color.FromArgb(255, 128, 0);
+            Menos.ForeColor = SystemColors.ControlText;
+            Menos.Location = new Point(614, 122);
+            Menos.Name = "Menos";
+            Menos.Size = new Size(83, 38);
+            Menos.TabIndex = 1;
+            Menos.Text = "-";
+            Menos.UseVisualStyleBackColor = false;
+            // 
+            // Vezes
+            // 
+            Vezes.BackColor = Color.FromArgb(255, 128, 0);
+            Vezes.ForeColor = SystemColors.ControlText;
+            Vezes.Location = new Point(614, 205);
+            Vezes.Name = "Vezes";
+            Vezes.Size = new Size(83, 38);
+            Vezes.TabIndex = 2;
+            Vezes.Text = "x";
+            Vezes.UseVisualStyleBackColor = false;
+            // 
+            // Dividido
+            // 
+            Dividido.BackColor = Color.FromArgb(255, 128, 0);
+            Dividido.ForeColor = SystemColors.ControlText;
+            Dividido.Location = new Point(614, 281);
+            Dividido.Name = "Dividido";
+            Dividido.Size = new Size(83, 38);
+            Dividido.TabIndex = 3;
+            Dividido.Text = "÷";
+            Dividido.UseVisualStyleBackColor = false;
+            // 
+            // igual
+            // 
+            igual.BackColor = Color.FromArgb(255, 128, 0);
+            igual.ForeColor = SystemColors.ControlText;
+            igual.Location = new Point(614, 357);
+            igual.Name = "igual";
+            igual.Size = new Size(83, 38);
+            igual.TabIndex = 4;
+            igual.Text = "=";
+            igual.UseVisualStyleBackColor = false;
+            // 
+            // CE
+            // 
+            CE.BackColor = Color.FromArgb(255, 128, 0);
+            CE.ForeColor = SystemColors.ControlText;
+            CE.Image = (Image)resources.GetObject("CE.Image");
+            CE.ImageAlign = ContentAlignment.TopLeft;
+            CE.Location = new Point(30, 54);
+            CE.Name = "CE";
+            CE.Size = new Size(83, 38);
+            CE.TabIndex = 5;
+            CE.Text = "CE";
+            CE.UseVisualStyleBackColor = false;
+            // 
+            // Porcentagem
+            // 
+            Porcentagem.BackColor = Color.FromArgb(255, 128, 0);
+            Porcentagem.ForeColor = SystemColors.ControlText;
+            Porcentagem.Location = new Point(222, 54);
+            Porcentagem.Name = "Porcentagem";
+            Porcentagem.Size = new Size(83, 38);
+            Porcentagem.TabIndex = 6;
+            Porcentagem.Text = "%";
+            Porcentagem.UseVisualStyleBackColor = false;
+            // 
+            // Num1
+            // 
+            Num1.BackColor = Color.FromArgb(255, 128, 0);
+            Num1.ForeColor = SystemColors.ControlText;
+            Num1.Location = new Point(30, 135);
+            Num1.Name = "Num1";
+            Num1.Size = new Size(83, 38);
+            Num1.TabIndex = 7;
+            Num1.Text = "1";
+            Num1.UseVisualStyleBackColor = false;
+            // 
+            // Num2
+            // 
+            Num2.BackColor = Color.FromArgb(255, 128, 0);
+            Num2.ForeColor = SystemColors.ControlText;
+            Num2.Location = new Point(222, 122);
+            Num2.Name = "Num2";
+            Num2.Size = new Size(83, 38);
+            Num2.TabIndex = 8;
+            Num2.Text = "2";
+            Num2.UseVisualStyleBackColor = false;
+            // 
+            // Num3
+            // 
+            Num3.BackColor = Color.FromArgb(255, 128, 0);
+            Num3.ForeColor = SystemColors.ControlText;
+            Num3.Location = new Point(415, 122);
+            Num3.Name = "Num3";
+            Num3.Size = new Size(83, 38);
+            Num3.TabIndex = 9;
+            Num3.Text = "3";
+            Num3.UseVisualStyleBackColor = false;
+            // 
+            // Num4
+            // 
+            Num4.BackColor = Color.FromArgb(255, 128, 0);
+            Num4.ForeColor = SystemColors.ControlText;
+            Num4.Location = new Point(50, 215);
+            Num4.Name = "Num4";
+            Num4.Size = new Size(83, 38);
+            Num4.TabIndex = 10;
+            Num4.Text = "4";
+            Num4.UseVisualStyleBackColor = false;
+            // 
+            // Num5
+            // 
+            Num5.BackColor = Color.FromArgb(255, 128, 0);
+            Num5.ForeColor = SystemColors.ControlText;
+            Num5.Location = new Point(222, 215);
+            Num5.Name = "Num5";
+            Num5.Size = new Size(83, 38);
+            Num5.TabIndex = 11;
+            Num5.Text = "5";
+            Num5.UseVisualStyleBackColor = false;
+            // 
+            // Num6
+            // 
+            Num6.BackColor = Color.FromArgb(255, 128, 0);
+            Num6.ForeColor = SystemColors.ControlText;
+            Num6.Location = new Point(415, 215);
+            Num6.Name = "Num6";
+            Num6.Size = new Size(83, 38);
+            Num6.TabIndex = 12;
+            Num6.Text = "6";
+            Num6.UseVisualStyleBackColor = false;
+            // 
+            // Num7
+            // 
+            Num7.BackColor = Color.FromArgb(255, 128, 0);
+            Num7.ForeColor = SystemColors.ControlText;
+            Num7.Location = new Point(50, 322);
+            Num7.Name = "Num7";
+            Num7.Size = new Size(83, 38);
+            Num7.TabIndex = 13;
+            Num7.Text = "7";
+            Num7.UseVisualStyleBackColor = false;
+            // 
+            // Num8
+            // 
+            Num8.BackColor = Color.FromArgb(255, 128, 0);
+            Num8.ForeColor = SystemColors.ControlText;
+            Num8.Location = new Point(222, 322);
+            Num8.Name = "Num8";
+            Num8.Size = new Size(83, 38);
+            Num8.TabIndex = 14;
+            Num8.Text = "8";
+            Num8.UseVisualStyleBackColor = false;
+            // 
+            // Num9
+            // 
+            Num9.BackColor = Color.FromArgb(255, 128, 0);
+            Num9.ForeColor = SystemColors.ControlText;
+            Num9.Location = new Point(415, 322);
+            Num9.Name = "Num9";
+            Num9.Size = new Size(83, 38);
+            Num9.TabIndex = 15;
+            Num9.Text = "9";
+            Num9.UseVisualStyleBackColor = false;
+            // 
+            // Num0
+            // 
+            Num0.BackColor = Color.FromArgb(255, 128, 0);
+            Num0.ForeColor = SystemColors.ControlText;
+            Num0.Location = new Point(415, 54);
+            Num0.Name = "Num0";
+            Num0.Size = new Size(83, 38);
+            Num0.TabIndex = 16;
+            Num0.Text = "0";
+            Num0.UseVisualStyleBackColor = false;
+            // 
+            // Resultado
+            // 
+            Resultado.BackColor = Color.White;
+            Resultado.Location = new Point(242, 2);
+            Resultado.Multiline = true;
+            Resultado.Name = "Resultado";
+            Resultado.Size = new Size(305, 46);
+            Resultado.TabIndex = 17;
+            // 
+            // Calculadora
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(800, 450);
+            Controls.Add(Resultado);
+            Controls.Add(Num0);
+            Controls.Add(Num9);
+            Controls.Add(Num8);
+            Controls.Add(Num7);
+            Controls.Add(Num6);
+            Controls.Add(Num5);
+            Controls.Add(Num4);
+            Controls.Add(Num3);
+            Controls.Add(Num2);
+            Controls.Add(Num1);
+            Controls.Add(Porcentagem);
+            Controls.Add(CE);
+            Controls.Add(igual);
+            Controls.Add(Dividido);
+            Controls.Add(Vezes);
+            Controls.Add(Menos);
+            Controls.Add(Mais);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Calculadora";
+            Text = "Calculadora";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button Mais;
+        private Button Menos;
+        private Button Vezes;
+        private Button Dividido;
+        private Button igual;
+        private Button CE;
+        private Button Porcentagem;
+        private Button Num1;
+        private Button Num2;
+        private Button Num3;
+        private Button Num4;
+        private Button Num5;
+        private Button Num6;
+        private Button Num7;
+        private Button Num8;
+        private Button Num9;
+        private Button Num0;
+        private TextBox Resultado;
+    }
+}
--- Carregando.cs
+++ Carregando.cs
@@ -1,14 +1,22 @@
+using System.Diagnostics.Metrics;
+
 namespace Calculadora
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
+
             InitializeComponent();
+           
+
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
+            Calculadora calculadora = new Calculadora();
+            calculadora.Show();
+            this.Hide();
 
         }
     }
